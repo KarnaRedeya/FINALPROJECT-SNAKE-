159566c996307577dd5235c8fbef10352c9067c9
KarnaRedeya <66736486+KarnaRedeya@users.noreply.github.com>
2021-06-23 11:50:15 +0700

    Update snake.java

--- snake/src/snake2/snake.java
+++ snake/src/snake2/snake.java
@@ -12,14 +12,14 @@ import java.util.ArrayList;
 public class snake extends JFrame implements  KeyListener, ActionListener
 {
 
-     static String head;     //direction of head
-     static int foodx;         //x point of the food
-     static int foody;         //y point of the food
+     static String head;       //direction of head
+     static int makananx;         //coordinates of x point of thmakanan
+     static int makanany;         //coordinates of y point of thmakanan
 
      static ArrayList<Integer>  arrayx;      //to store x point of the snake
-     static ArrayList<Integer> arrayy;//to store y point of the snake
-     Timer timer;
-     int delay=100;
+     static ArrayList<Integer>  arrayy;     //to store y point of the snake
+     Timer waktu;
+     int delay=100;                        //Speed of snake
      static algorithm obj=new algorithm();
     public snake()
     {
@@ -27,12 +27,14 @@ public class snake extends JFrame implements  KeyListener, ActionListener
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        timer=new Timer(delay,this);
-        timer.start();
-        arrayx = new ArrayList<Integer>();
-        arrayy =  new ArrayList<Integer>();
-        foodx= (int) (Math.random()*90+5)*5;
-        foody=(int) (Math.random()*90+5)*5;
+        waktu=new Timer(delay,this);
+        waktu.start();
+          arrayx = new ArrayList<Integer>();
+        arrayy = new ArrayList<Integer>();
+        makananx= (int) (Math.random()*90+5)*5; //why use 5? because the size of the snake is 5 if lower it  
+        makanany= (int) (Math.random()*90+5)*5; //will not be able to eat thmakanan of more snake will not come close to thmakanan
+      
+
         for (int i = 0; i < 10; i++) {
             arrayx.add(250-5*i);
 
@@ -51,7 +53,7 @@ public class snake extends JFrame implements  KeyListener, ActionListener
 
         snake jf=new snake();
         JFrame j=new JFrame();
-        jf.setTitle("Snake game");
+        jf.setTitle("Snake Test");
         jf.setVisible(true);
 
         jf.setSize(500,500);
@@ -61,20 +63,20 @@ public class snake extends JFrame implements  KeyListener, ActionListener
         @Override
     public void paint(Graphics g)
     {
-        head=obj.a_star(arrayx, arrayy, foodx, foody);  //takes the next direction for snake from A* algorithm class
-        g.setColor(Color.BLACK);
+        head=obj.a_star(arrayx, arrayy, makananx, makanany);                  //takes the next direction for snake from A* algorithm class
+        g.setColor(Color.BLACK);                                        //Colour of background
         g.fillRect(0,0,500,500);
-        if(arrayx.get(0)==foodx && arrayy.get(0)==foody) {
-            foodx= (int) (Math.random()*90+5)*5;
-            foody=(int) (Math.random()*90+5)*5;
-            System.out.println(foodx+" "+foody);
+        if(arrayx.get(0)==makananx && arrayy.get(0)==makanany) {
+            makananx= (int) (Math.random()*90+5)*5;                        //code ini di gunakan untuk setiap kali snakenya makan maka akan randomize makanan baru 
+            makanany=(int) (Math.random()*90+5)*5;
+            System.out.println(makananx+" "+makanany);
 
-            arrayy.add(arrayy.get(arrayy.size()-1));
+            arrayy.add(arrayy.get(arrayy.size()-1));                    //menambahkan index supaya size snake akan membesar
             arrayx.add(arrayx.get(arrayx.size()-1));
 
         }
 
-        for(int i=0;i<arrayx.size();i++)
+        for(int i=0;i<arrayx.size();i++) //Paint snake
         {
 
             g.setColor(Color.white);
@@ -83,7 +85,7 @@ public class snake extends JFrame implements  KeyListener, ActionListener
 
 
 
-        g.fillRect(foodx,foody,5,5);
+        g.fillRect(makananx,makanany,5,5); //makanan colour
 
 
     }
@@ -110,14 +112,8 @@ public class snake extends JFrame implements  KeyListener, ActionListener
     @Override
     public void actionPerformed(ActionEvent actionEvent)
     {
-
-
-
-
         if(head.equals("right"))
         {
-
-
             for (int i = arrayx.size() - 1; i >= 1; i--)
             {
 
